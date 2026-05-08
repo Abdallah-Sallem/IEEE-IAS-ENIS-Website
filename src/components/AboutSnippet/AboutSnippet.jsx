e664ba3c1c24ccbda9eee419a4421a9705c2a335
Abdallah-Sallem <abdallahsallem9@gmail.com>
2026-05-08 02:44:55 +0100

    removed webp files

--- src/components/AboutSnippet/AboutSnippet.jsx
+++ src/components/AboutSnippet/AboutSnippet.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import { useInView } from '../../hooks/useInView';
 import styles from './AboutSnippet.module.css';
 
-const IAS_LOGO = '/assets/iaslogo.png';
+const IAS_LOGO = '/assets/iaslogo.jpg';
 
 const fadeLeft = {
   hidden: { opacity: 0, x: -50 },
